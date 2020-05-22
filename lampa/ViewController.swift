67f66339d82d7e18da248e942cdbec8e3559ccb5
pogosian1995 <pogosian1995@gmail.com>
2020-05-22 23:37:20 +0300

    Initial Commit

---
+++ lampa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lampa
+//
+//  Created by Oleg Pogosian on 22.05.2020.
+//  Copyright © 2020 Oleg Pogosian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
